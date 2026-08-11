d0857c1f8037213f1f3e745d9f16f6d589deb7c0
Marcio Baptista Fidalgo da Costa <marcio.fidalgo@al.infnet.edu.br>
2026-08-11 09:19:55 -0300

    criação do DbContexto

---
+++ Data/AppDbContext.cs.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
+namespace backend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppContext> options) : base(options) { }
+
+        public DbSet<Agendamento> Servico {get; set;}
+
+        public DbSet<Cliente> Cliente { get; set; }
+
+        public DbSet<Servico> Agendamento { get; set; }
+    }
+}
